5d12a4d2f56abd6e656a8872913e458b679569ae
Shardul <kumbharshardul384@gmail.com>
2022-08-12 13:03:58 +0530

    [Shardul] Add . Counter increment Project by TTD in rect

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import TestAxios from "./components/testAxios/TestAxios";
+
+function App() {
+
+  // return <Counter></Counter>
+  // return <AsynchCounter/>
+  return <TestAxios url='https://api.github.com/users/ShardulKumbhar'></TestAxios>
+}
+
+export default App;
